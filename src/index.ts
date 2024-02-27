e88c20667231bbe8e3d9c0ce1788d2a9dfbecc23
Aervyon <aervy@aervyon.com>
2024-02-26 20:08:30 -0600

    fix(src/index.ts): Let's just make the url optional

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ export * from './helpers/request-handlers';
 
 import * as Manage from './management/index';
 
-export function setup(url: string): { Manage: typeof Manage } {
-    if (url === '') {
+export function setup(url?: string): { Manage: typeof Manage } {
+    // we'll just assume this is relative
+    if (url === '' || !url) {
         return { Manage: Manage }
     }
 
